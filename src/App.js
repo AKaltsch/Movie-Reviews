98c83acf818acdcedce6df7ff1739920541a13ef
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-07-20 23:25:10 -0700

    Car Reviews

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [carName, setCarName] = useState("");
+  const [carReview, setCarReview] = useState("");
+
+  const submitReview = () => {
+    Axios.post("http://localhost:4000/api/insert", {
+      carName: carName,
+      carReview: carReview,
+    }).then(() => {
+      console.log("promise is returning");
+      alert("successful insert");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Application</h1>
+      <div className="form">
+        <label>Car Name</label>
+        <input
+          type="text"
+          name="carName"
+          onChange={(e) => setCarName(e.target.value)}
+        />
+        <label>Review</label>
+        <input
+          type="text"
+          name="carReview"
+          onChange={(e) => setCarReview(e.target.value)}
+        />
+
+        <button onClick={submitReview}>Submit</button>
+      </div>
     </div>
   );
 }
